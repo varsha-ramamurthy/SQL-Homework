4daf30d007cb0b1bf0f49e30c689147898e3f59a
varsha-ramamurthy <50121685+varsha-ramamurthy@users.noreply.github.com>
2019-07-13 18:45:19 -0400

    SQL Homework
    SQL Homework minus Bonus question completed

---
+++ SQL Homework/ph_hw.sql
@@ -0,0 +1,133 @@
+-- Database: ph_hw_db
+
+-- DROP DATABASE ph_hw_db;
+
+CREATE DATABASE ph_hw_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+-- create employees table
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees
+(
+	emp_no INT PRIMARY KEY NOT NULL,
+	birth_date VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(30) NOT NULL
+);
+
+-- create departments table
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments
+(
+	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+-- create dept_emp table
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+-- create dept_manager table
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager
+(
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+-- create salaries table
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INT NOT NULL,
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+-- create titles table
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles
+(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	title VARCHAR(30) NOT NULL,
+	from_date VARCHAR(30) NOT NULL,
+	to_date VARCHAR(30) NOT NULL
+);
+
+-- List the employee number, last name, first name, gender, and salary for each employee
+SELECT employees.emp_no, employees.first_name, employees.last_name, employees.gender, salaries.salary
+FROM employees
+INNER JOIN salaries ON employees.emp_no=salaries.emp_no;
+
+--List employees who were hired in 1986
+SELECT * FROM employees
+WHERE hire_date LIKE '1986%';
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates
+SELECT d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS m ON
+m.dept_no = d.dept_no
+JOIN employees AS e ON
+e.emp_no = m.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, dept.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON
+e.emp_no = de.emp_no
+INNER JOIN departments AS dept ON
+dept.dept_no = de.dept_no;
+
+--List all employees whose first name is "Hercules" and last names begin with "B"
+SELECT * FROM employees
+WHERE first_name LIKE 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, dept.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON
+e.emp_no = de.emp_no
+INNER JOIN departments AS dept ON
+dept.dept_no = de.dept_no
+WHERE dept.dept_name LIKE 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, dept.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON
+e.emp_no = de.emp_no
+INNER JOIN departments AS dept ON
+dept.dept_no = de.dept_no
+WHERE dept.dept_name LIKE 'Sales'
+OR dept.dept_name LIKE 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
